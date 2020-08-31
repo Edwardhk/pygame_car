90bd2f8082ccf452667cfc15308211e0a2ed3d00
Edward <hkm727145@gmail.com>
2020-08-31 23:37:52 +0800

    [INFO] Added car info, added viewing angle

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 import pygame
-import pygame.gfxdraw
 import numpy as np
 
 # Display & color
-BG = (40, 42, 54)
+BG = (32, 34, 43)
 FG = (248, 248, 242)
+BLACK = (98, 114, 164)
 GREY = (68, 71, 90)
 PINK = (255, 121, 198)
 GREEN = (80, 250, 123)
 PURPLE = (189, 147, 249)
 WHITE = (248, 248, 242)
-SCREEN_RESOLUTION = (1200, 800)
+SCREEN_RESOLUTION = (1400, 800)
 
 # Car config
+INIT_POSITION = (200, 650)
 CAR_HEIGHT = 20
 CAR_WIDTH = 10
+
+ACCELERATION = 0.3
+BRAKE = 0.5
+INIT_SPEED = 1
 MAX_SPEED = 10
-ROTATE_ANGLE = 8
-INIT_POSITION = (200, 650)
+
+ROTATE_ANGLE = 5
 IN_ANGLE = np.rad2deg(np.arctan(CAR_WIDTH / CAR_HEIGHT))
 
 
 class Car:
     def __init__(self):
-        self.speed = 1
-        self.angle = 0
+        self.speed = INIT_SPEED
         self.height = CAR_HEIGHT
         self.width = CAR_WIDTH
         self.off = pygame.math.Vector2(INIT_POSITION)
@@ -36,14 +40,14 @@ class Car:
         elif keys[pygame.K_RIGHT]:
             self.vel.rotate_ip(ROTATE_ANGLE)
         if keys[pygame.K_UP]:
-            self.speed = MAX_SPEED if self.speed + 1 > MAX_SPEED else self.speed + 1
+            self.speed = MAX_SPEED if self.speed + \
+                ACCELERATION > MAX_SPEED else self.speed + ACCELERATION
         elif keys[pygame.K_DOWN]:
-            self.speed = 0 if self.speed <= 3 else self.speed - 3
+            self.speed = 0 if self.speed <= BRAKE else self.speed - BRAKE
 
         # Reset
         if keys[pygame.K_r]:
-            self.speed = 1
-            self.angle = 0
+            self.speed = INIT_SPEED
             self.off = pygame.math.Vector2(INIT_POSITION)
             self.vel = pygame.math.Vector2(self.height / 2, 0)
 
@@ -63,23 +67,38 @@ class GameWorld:
     def render(self):
         self.win.fill(BG)
         for car in self.car_arr:
-            if car.speed < MAX_SPEED:
+            if car.speed < MAX_SPEED and car.speed != 0:
                 car.speed += 0.001
             edges_xy = car.get_polygon_edges()
 
             # Car wheel rendering
-            for edge in edges_xy:
-                pygame.draw.circle(
-                    self.win, PINK, (int(edge[0]), int(edge[1])), 2)
+            # for edge in edges_xy:
+            #     pygame.draw.circle(
+            #         self.win, GREEN, (int(edge[0]), int(edge[1])), 3)
 
             # Car body rendering
             car.off += car.vel.normalize() * car.speed
-            pygame.draw.polygon(self.win, PURPLE, edges_xy)
+            pygame.draw.polygon(self.win, BLACK, edges_xy)
 
             # Simulate the angle of sensor
-            pygame.draw.line(self.win, GREEN, car.off,
+            pygame.draw.line(self.win, PINK, car.off,
                              car.off + car.vel * 2, 2)
+            pygame.draw.line(self.win, PINK, car.off,
+                             car.off + car.vel.rotate(62) * 4, 1)
+            pygame.draw.line(self.win, PINK, car.off,
+                             car.off + car.vel.rotate(-62) * 4, 1)
+
             pygame.display.update()
+
+    def show_info(self):
+        FONT_SIZE = 18
+        car_info = pygame.font.SysFont('Consolas', FONT_SIZE)
+        for car in self.car_arr:
+            self.win.blit(car_info.render(
+                f'Car#0 speed: {car.speed:.3f}/{MAX_SPEED}', 1, GREEN), (0, 0))
+            self.win.blit(car_info.render(
+                f'Car#0 velocity:  {car.vel}', 1, GREEN), (0, FONT_SIZE))
+        pygame.display.update()
 
 
 if __name__ == "__main__":
@@ -100,3 +119,4 @@ if __name__ == "__main__":
         if sum(keys) != 0:
             car0.move(keys)
         game_world.render()
+        game_world.show_info()
